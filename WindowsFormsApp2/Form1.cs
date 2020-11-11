e1a505e84cfbc40b193c88c781b5f8b3df10bfbc
好帥 我 <g0950128@yahoo.com.tw>
2020-11-12 01:39:11 +0800

    加入專案檔案。

---
+++ WindowsFormsApp2/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string input_int = textBox1.Text;
+            //轉換int 
+            MD5 md5 = MD5.Create();//建立一個MD5
+            byte[] source = Encoding.Default.GetBytes(input_int);//將字串轉為Byte[]
+            byte[] crypto = md5.ComputeHash(source);//進行MD5加密
+            string result = Convert.ToBase64String(crypto);//把加密後的字串從Byte[]轉為字串
+            MessageBox.Show($"{result}", "姓名成分分析", MessageBoxButtons.OK);
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
